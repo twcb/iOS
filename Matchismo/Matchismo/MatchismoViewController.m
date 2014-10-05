de89415e6d1959b76355c618ac3c1a4ffa926dcd
Carson Brown <carson.brown@lc.cuny.edu>
2014-10-05 14:16:57 -0400

    Storyboard almost complete

--- Matchismo/Matchismo/MatchismoViewController.m
+++ Matchismo/Matchismo/MatchismoViewController.m
@@ -7,23 +7,60 @@
 //
 
 #import "MatchismoViewController.h"
+#import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface MatchismoViewController ()
-
+@property (nonatomic, strong) Deck *deck;
+@property (nonatomic, strong) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation MatchismoViewController
 
-- (void)viewDidLoad
+- (CardMatchingGame *)game
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                          usingDeck:[self createDeck]];
+    return _game;
 }
 
-- (void)didReceiveMemoryWarning
+- (Deck *)createDeck
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return [[PlayingCardDeck alloc] init];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    NSUInteger cardIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:cardIndex];
+    [self updateUI];
+}
+
+- (void)updateUI
+{
+    for (UIButton *cardButton in self.cardButtons) {
+        NSUInteger cardIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardIndex];
+        
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card]
+                              forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+    }
+    
+    // %ld and explicit typecast to long to support 32bit as well as 64bit
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld", (long) self.game.score];
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return (card.isChosen) ? card.contents : @"";
+}
+
+- (UIImage *)backgroundImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:(card.isChosen) ? @"cardFront" : @"cardBack"];
 }
 
 @end
